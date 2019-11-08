c4e6edc7146f70d4a4e03beaf4c02608ea004f09
chefinDan <greendan@oregonstate.edu>
2019-11-08 15:38:36 -0800

    env_type passed on cli, npm run dev for local, npm start for gcp

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,12 @@
 const uuid = require('uuid/v1')
 const axios = require('axios')
-const dataService = require('../service/dataService')
 const peopleController = require('../controller/peopleController')
 const clientId = require('../client_id.json').web
 const scope = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+
+const redirect_uri = (global.env_type === 'local') ? clientId.redirect_uris[1] : clientId.redirect_uris[0] 
 var state;
+
 exports.check = async (req, res, next) => {
     console.log('GET /people')
     console.log("Checking if user has authorized google")        
@@ -12,7 +14,7 @@ exports.check = async (req, res, next) => {
         
         console.log("Access token not found")
         state = uuid();
-        let oauth_uri = `${clientId.auth_uri}?client_id=${clientId.client_id}&redirect_uri=${clientId.redirect_uris[1]}&scope=${scope}&response_type=code&state=${state}`
+        let oauth_uri = `${clientId.auth_uri}?client_id=${clientId.client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=code&state=${state}`
         
         console.log("Sending oauth uri: ", oauth_uri)
         console.log("Presenting user with google oauth link")
@@ -27,15 +29,14 @@ exports.check = async (req, res, next) => {
 exports.redirect = async (req, res, next) => {
     console.log('GET /auth/redirect')
     let query = req.query
-    let token_uri = `${clientId.token_uri}?code=${query.code}&client_id=${clientId.client_id}&client_secret=${clientId.client_secret}&redirect_uri=${clientId.redirect_uris[1]}&grant_type=authorization_code`
+    let token_uri = `${clientId.token_uri}?code=${query.code}&client_id=${clientId.client_id}&client_secret=${clientId.client_secret}&redirect_uri=${redirect_uri}&grant_type=authorization_code`
     console.log("User authorized google")
-    console.log("Making access_token request to: ", token_uri)
     try {
         let token_res = await axios.post(clientId.token_uri, {
             'code': query.code,
             'client_id': clientId.client_id,
             'client_secret': clientId.client_secret,
-            'redirect_uri': clientId.redirect_uris[1],
+            'redirect_uri': redirect_uri,
             'grant_type': 'authorization_code'
         })
         console.log(token_res.data)
--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 const PORT = process.env.PORT || 8080;
+global.env_type = process.argv[2]
 
 app.listen(PORT, function () {
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
+    if(env_type === "local")
+        console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
 });
